eee8f98782536c51bdfcd1870a5e30867a4f5b11
RAZİYE DOGAN <98969893+raziyedogan@users.noreply.github.com>
2023-02-18 15:19:27 +0300

    Add files via upload

---
+++ pointers_and_strings/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void putstr(char *s){
+	int t;
+	
+	for(t=0; s[t]; t++)
+		putchar(s[t]);
+}
+
+void _putstr(char *s){
+	while(*s)
+		putchar(*s++);
+}
+
+int main(){
+	char str[]="the coffee";
+	putstr(str);
+	printf("\n");
+	_putstr(str);
+}
+
+//Profesyonel C programcýlarýnýn çoðu ikinci versiyonu daha anlaþýlýr bulmaktadýr ve ilk versiyona göre daha sýklýkla kullanýlmaktadýr.
